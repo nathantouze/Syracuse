20429fbdfbf5184590f5299ce74c5c8ecf5c9e68
nathantouze <nathan.touze@epitech.eu>
2022-03-03 19:35:15 +0100

    {master}: added time management

--- src/Syracuse.cpp
+++ src/Syracuse.cpp
@@ -1,6 +1,7 @@
 #include "Syracuse.hpp"
 #include <fstream>
 #include <iostream>
+#include <chrono>
 
 bool Syracuse::loopProcess(StringNum startingNumber) {
 
@@ -17,6 +18,8 @@ bool Syracuse::loopProcess(StringNum startingNumber) {
 bool Syracuse::process(StringNum number) {
 
     std::fstream file;
+    std::clock_t start = std::clock();
+    std::clock_t end;
 
     if (_output == OUTPUT_FILE) {
         file.open("./output/" + number.toString() + ".txt", std::ios::out);
@@ -39,11 +42,12 @@ bool Syracuse::process(StringNum number) {
             }
         }
         if (number.toLong() == 4 || number.toLong() == 2 || number.toLong() == 1) {
+            end = clock();
             if (_output == OUTPUT_FILE) {
-                file << "syracuse conjecture failed." << std::endl;
+                file << "syracuse conjecture failed. (time: " << ((float)end - start) / CLOCKS_PER_SEC << " s)" << std::endl;
                 file.close();
             } else {
-                std::cout << "failed." << std::endl;
+                std::cout << "failed. (time: " << ((float)end - start) / CLOCKS_PER_SEC << " s)" << std::endl;
             }
             return false;
         }
